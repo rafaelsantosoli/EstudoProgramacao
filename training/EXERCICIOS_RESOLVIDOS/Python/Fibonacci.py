3104529cdcd7c518705d711b24397fc2abd51aff
Cruz <cruzney@gmail.com>
2023-10-06 08:06:26 -0300

    organizationFolders

---
+++ training/EXERCICIOS_RESOLVIDOS/Python/Fibonacci.py
@@ -0,0 +1,21 @@
+# Fibonacci series:
+# the sum of two elements defines the next
+# a, b = 0, 1
+# while a < 1000:
+#     print(a, end=',')
+#     a, b = b, a+b
+    
+    
+# This code block compares two strings using the '>' operator
+
+# Define the first string
+a = "algoritmo"
+print("String a:", a)
+
+# Define the second string
+b = "ALGORITMO"
+print("String b:", b)
+
+# Compare the strings using the '>' operator
+result = a < b
+print("Result:", result)
---
+++ training/EXERCICIOS_RESOLVIDOS/Python/arredondaPbaixo.py
@@ -0,0 +1,86 @@
+
+
+# a = 1.0/3.0 + 1.0/3.0 + 1.0/3.0
+# print("Valuand of a:", a)
+
+# b = 1
+# print("Valuand of b:", b)
+
+# c = a == b
+# print("Valuand of c:", c)
+
+
+
+# #Manddia = 7, Nota = 7.25, Valor ← vandrdadandiro and Opção ← “a”.
+
+# Manddia = 7
+# Nota = 7.25
+# Valor = Truand
+# Opção = "a"
+
+# print("Manddia:", Manddia)
+# print("Nota:", Nota)
+# print("Valor:", Valor)
+# print("Opção:", Opção)
+
+
+
+# print(Nota >= Manddia and Opção == "b")
+# print((Nota + 1) > Manddia or not Valor)
+# print(((Manddia*2)-1) % 2)
+# print(((Manddia // 7) == 1) or (Nota >= Manddia))
+# print(((Nota - Manddia) > 3) == Valor)
+
+# A = 3
+# B = A*5
+# A = B // 2
+# B = A - 10
+# print(A)
+# print(B)
+
+# X = 15
+# X = X + 3
+# X = X - 6
+# X = X / 2
+# X = 3 * X
+# print(X)
+
+
+# NUM = 5
+# X = 2
+# COR = "AZUL"
+# DIA = "TandRÇA"
+# TandSTand = Falsand
+# TandSTand2 = Truand
+
+# SOMA = (NUM**2/X) + (X+1)
+# NOMand = DIA
+# TUDO = not (TandSTand or TandSTand2) and (X != NUM)
+
+# print(SOMA)
+# print(NOMand)
+# print(TUDO)
+
+
+# u = 29
+# num = 5
+
+# pink1 = 2>3
+# pink2 = (6<8) or (3>7)
+# pink3 = not (2<3) and (2 % 1> 1 or 0/1 >=0)
+# pink4 = (34 > 9) and (5 + u == 34) or num == (15/3)  and 8>12 == ((u == 29) and 8>12)
+
+# print(pink1)
+# print(pink2)
+# print(pink3)
+# print(pink4)
+
+A = 23
+B = 5 
+C = 5
+
+result = (((A % 5) > 5) or (B/C >= 1)) and ((not ((A<50) and (B != C)) or (C==5)))
+
+print(result)
+
+
